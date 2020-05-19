8826ae537799abcab31216975f4a27f5663c1744
Prabhukanagaraj <65549136+Prabhukanagaraj@users.noreply.github.com>
2020-05-19 12:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
 console.log('You have connected...')
-
-document.addEventListener("DOMContentLoaded", () =>{
-
-    let generateBtn = document.querySelector('#generate-pokemon');
-    generateBtn.addEventListener('click', renderEverything)
-})
-
-function renderEverything(){
+function renderEverything() {
+    var name;
+    name = (document.getElementById("userName").value);
     let allPokemonContainer = document.querySelector('#poke-container')
     allPokemonContainer.innerText = "";
-    fetchKPokemon();
+    fetchKPokemon(name);
 }
 
-function fetchKPokemon(){
+function fetchKPokemon(name) {
+    var userName = name;
     fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
-    .then(response => response.json())
-    .then(function(allpokemon){
-        allpokemon.results.forEach(function(pokemon){
-            fetchPokemonData(pokemon);
+        .then(response => response.json())
+        .then(function (allpokemon) {
+            allpokemon.results.forEach(function (pokemon)  {
+                if (userName[0] == pokemon.name[0]) {
+                    fetchPokemonData(pokemon);
+                }
+            })
         })
-    })
 }
 
-function fetchPokemonData(pokemon){
-    let url = pokemon.url 
+function fetchPokemonData(pokemon) {
+    let url = pokemon.url
     fetch(url)
-    .then(response => response.json())
-    .then(function(pokeData){
-        renderPokemon(pokeData)
-    })
+        .then(response => response.json())
+        .then(function (pokeData) {
+            renderPokemon(pokeData)
+        })
 }
 
 
-function renderPokemon(pokeData){
+function renderPokemon(pokeData) {
     let allPokemonContainer = document.getElementById('poke-container');
-    let pokeContainer = document.createElement("div") 
+    let pokeContainer = document.createElement("div")
     pokeContainer.classList.add('ui', 'card');
 
     createPokeImage(pokeData.id, pokeContainer);
 
-    let pokeName = document.createElement('h4') 
+    let pokeName = document.createElement('h4')
     pokeName.innerText = pokeData.name
 
     let pokeNumber = document.createElement('p')
     pokeNumber.innerText = `#${pokeData.id}`
-   
-    let pokeTypes = document.createElement('ul') 
-  
 
-    createTypes(pokeData.types, pokeTypes) 
+    let pokeTypes = document.createElement('ul')
 
-    pokeContainer.append(pokeName, pokeNumber, pokeTypes);  
-    allPokemonContainer.appendChild(pokeContainer);  
+
+    createTypes(pokeData.types, pokeTypes)
+
+    pokeContainer.append(pokeName, pokeNumber, pokeTypes);
+    allPokemonContainer.appendChild(pokeContainer);
 }
 
-function createTypes(types, ul){
-    types.forEach(function(type){
+function createTypes(types, ul) {
+    types.forEach(function (type) {
         let typeLi = document.createElement('li');
         typeLi.innerText = type['type']['name'];
         ul.append(typeLi)
     })
 }
 
-function createPokeImage(pokeID, containerDiv){
+function createPokeImage(pokeID, containerDiv) {
     let pokeImgContainer = document.createElement('div')
     pokeImgContainer.classList.add('image')
 
